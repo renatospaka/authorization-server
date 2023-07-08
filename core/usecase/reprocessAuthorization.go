22ec9a797c6829dc0b0fbaefe4b4a0942f895c73
Renato Spakauskas <renatospaka@gmail.com>
2023-07-08 13:26:29 -0300

    feat: :necktie: reprocess usecase
    enforcing business rules atthe usecase level

--- core/usecase/reprocessAuthorization.go
+++ core/usecase/reprocessAuthorization.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/renatospaka/authorization-server/core/dto"
@@ -11,21 +12,38 @@ import (
 func (a *AuthorizationUsecase) ReprocessAuthorization(auth *dto.AuthorizationReprocessDto) (*dto.AuthorizationReprocessResultDto, error) {
 	log.Println("usecase.authorizations.reprocessAuthorization")
 
+	// These should be relying on entity validation
+	// if err := validateTransactionId(auth.TransactionID); err != nil {return nil, err}
+	// if err := validateClientId(auth.ClientID); err != nil {return nil, err}
+	// if err := validateAuthorizationId(auth.AuthorizationID); err != nil {return nil, err}
+
 	response := &dto.AuthorizationReprocessResultDto{
-		ClientID:      auth.ClientID,
-		TransactionID: auth.TransactionID,
-		Value:         auth.Value,
+		AuthorizationID: auth.AuthorizationID,
+		ClientID:        auth.ClientID,
+		TransactionID:   auth.TransactionID,
+		Value:           auth.Value,
 	}
 
-	authorization, err := entity.NewAuthorization(auth.ClientID, auth.TransactionID, auth.Value)
+	authorization, err := a.repo.FindById(auth.AuthorizationID)
 	if err != nil {
 		response.ErrorMessage = err.Error()
 		return response, err
 	}
 
-	authorization.Process()
-	response.AuthorizationID = authorization.GetID()
-	response.Status = authorization.GetStatus()
+	status := authorization.GetStatus()
+	if status != entity.TR_PENDING {
+		err := fmt.Errorf("cannot reprocess authorization with status %s", status)
+		response.ErrorMessage = err.Error()
+		return response, err
+	}
+
+	newStatus, err := authorization.Reprocess()
+	if err != nil {
+		response.ErrorMessage = err.Error()
+		return response, err
+	}
+
+	response.Status = newStatus
 	response.DeniedAt = dateTime.FormatDateToNull(authorization.DeniedAt())
 	response.ApprovedAt = dateTime.FormatDateToNull(authorization.ApprovedAt())
 	if !authorization.IsValid() {
